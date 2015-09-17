38a6dc2ff6984f84f8b555f69c4f4022c0f87134
Chris Cornutt <enygma@phpdeveloper.org>
2015-09-17 05:35:41 -0500

    more improvements for the checks

--- test.php
+++ test.php
@@ -6,20 +6,18 @@ class Enforcer
 {
     public function evaluate(User $subject, Policy $policy)
     {
-        echo '-------- EVAL'."\n";
-
         $pass = true;
 
         foreach ($policy->getChecks() as $type => $value) {
-            echo $type.' -> '.print_r($value, true)."\n";
+            // echo $type.' -> '.print_r($value, true)."\n";
 
             $propertyValue = $subject->getProperty($type);
-            echo 'property value: '.print_r($propertyValue, true)."\n";
+            // echo 'property value: '.print_r($propertyValue, true)."\n";
             $valueType = gettype($propertyValue);
 
             // Ensure all of the things in our policy are true
             foreach ($value as $test) {
-                echo 'test: '.print_r($test, true)."\n";
+                // echo 'test: '.print_r($test, true)."\n";
 
                 if ($valueType == 'string' && $this->testString($test, $propertyValue) === false) {
                     return false;
@@ -34,29 +32,105 @@ class Enforcer
 
     private function testArray($test, $compare)
     {
-        $value = $test[1];
+        $value = $test['value'];
 
-print_r($test);
-
-        if ($test[0] == 'equals') {
-            return (is_string($value)) ? (in_array($value, $compare)) : $test[1] == $compare;
-        } elseif ($test[0] == 'not-equals') {
+        if ($test['type'] == 'equals') {
             if (is_string($value)) {
-                return (!in_array($value, $compare));
+                if ($test['addl']['rule'] === Policy::ANY) {
+                    return in_array($value, $compare);
+                } elseif ($test['addl']['rule'] === Policy::ALL) {
+                    // ensure all items match
+                    foreach ($compare as $v) {
+                        if ($v !== $value) {
+                            return false;
+                        }
+                    }
+                }
             } elseif (is_array($value)) {
-                echo 'ARRAY';
+                if ($test['addl']['rule'] === Policy::ANY) {
+                    // see if any of the values match
+                    foreach ($compare as $v) {
+                        if ($v == $value) {
+                            return true;
+                        }
+                    }
+                } elseif ($test['addl']['rule'] === Policy::ALL) {
+                    // see if all values match
+                    return $compare == $value;
+                }
+            }
+
+        } elseif ($test['type'] == 'not-equals') {
+            if (is_string($value)) {
+                if ($test['addl']['rule'] === Policy::ANY) {
+                    return !in_array($value, $compare);
+                } elseif ($test['addl']['rule'] === Policy::ALL) {
+                    $fail = false;
+                    foreach ($compare as $v) {
+                        if ($v == $value && $fail === false) {
+                            $fail = true;
+                        }
+                    }
+                    // If we failed, return false
+                    return ($fail === true) ? false : true;
+                }
+
+                return (!in_array($value, $compare));
+
+            } elseif (is_array($value)) {
+                if ($test['addl']['rule'] === Policy::ANY) {
+
+                    // see if any of our values are in the array
+                    $fail = false;
+                    foreach ($value as $v) {
+                        if (in_array($v, $compare) && $fail == false) {
+                            $fail = true;
+                        }
+                    }
+                    // If we failed, return false
+                    return ($fail === true) ? false : true;
+
+                } elseif ($test['addl']['rule'] === Policy::ALL) {
+                    return $compare !== $value;
+                }
             } else {
-                return $test[1] !== $compare;
+                return $test['value'] !== $compare;
             }
         }
     }
     private function testString($test, $compare)
     {
-        $value = $test[1];
+        $value = $test['value'];
 
-        if ($test[0] == 'equals' && $compare !== $test[1]) {
+        if ($test['type'] == 'equals') {
+            if (is_array($value)) {
+                if ($test['addl']['rule'] === Policy::ANY) {
+                    return (in_array($compare, $value));
+                } elseif ($test['addl']['rule'] === Policy::ALL) {
+                    return $compare == $value;
+                }
+
+            } elseif (is_string($value)) {
+                // Comparing a string to a string
+                return $compare == $value;
+            }
+        } elseif ($test['type'] == 'not-equals') {
+            if (is_array($value)) {
+                if ($test['addl']['rule'] === Policy::ANY) {
+                    return (!in_array($compare, $value));
+                } elseif ($test['addl']['rule'] === Policy::ALL) {
+                    return $compare !== $value;
+                }
+
+            } elseif (is_string($value)) {
+                // Comparing a string to a string
+                return $compare !== $value;
+            }
+        }
+
+        if ($test['type'] == 'equals' && $compare !== $test['value']) {
             return false;
-        } elseif ($test[0] == 'not-equals' && $compare == $test[1]) {
+        } elseif ($test['type'] == 'not-equals' && $compare == $test['value']) {
             return false;
         }
 
@@ -68,23 +142,13 @@ class Policy
 {
     private $checks = [];
 
-    public function has($type, $value)
-    {
-        if (!isset($this->checks[$type])) {
-            $this->checks[$type] = [];
-        }
-        if (is_string($value)) {
-            $matchType = 'equals';
-        } elseif (is_array($value)) {
-            $matchType = 'contains';
-        }
-        $this->checks[$type][] = [$matchType, $value];
-        return $this;
-    }
+    const ANY = 'any';
+    const ALL = 'all';
+    const NONE = 'none';
 
     public function __call($name, $args)
     {
-        echo $name.' -> '.print_r($args, true)."\n";
+        echo 'CALL: '.$name.' -> '.print_r($args, true)."\n";
 
         // look for a "keyword"
         if (strpos($name, 'has') === 0) {
@@ -101,25 +165,41 @@ class Policy
     private function hasCheck($type, $name, $args)
     {
         $value = array_shift($args);
+        $matchType = 'equals';
 
-        if (is_string($value)) {
-            $matchType = 'equals';
-        } elseif (is_array($value)) {
-            $matchType = 'contains';
+        if (!isset($args[0])) {
+            $args[0] = ['rule' => Policy::ANY];
         }
-        $this->checks[$type][] = [$matchType, $value];
+
+        // see what other options we've been given
+        if (is_string($args[0]) && ($args[0] === Policy::ANY || $args[0] === Policy::ALL) ) {
+            $args['rule'] = $args[0];
+            unset($args[0]);
+        } elseif (is_array($args[0])) {
+            $args = $args[0];
+        }
+
+        $this->checks[$type][] = ['type' => $matchType, 'value' => $value, 'addl' => $args];
     }
     private function notCheck($type, $name, $args)
     {
         $value = array_shift($args);
-
-        // if (is_string($value)) {
-        //     $matchType = 'not-equals';
-        // } elseif (is_array($value)) {
-        //     $matchType = 'not-contains';
-        // }
         $matchType = 'not-equals';
-        $this->checks[$type][] = [$matchType, $value];
+
+
+        if (!isset($args[0])) {
+            $args[0] = ['rule' => Policy::ANY];
+        }
+
+        // see what other options we've been given
+        if (is_string($args[0]) && ($args[0] === Policy::ANY || $args[0] === Policy::ALL) ) {
+            $args['rule'] = $args[0];
+            unset($args[0]);
+        } elseif (is_array($args[0])) {
+            $args = $args[0];
+        }
+
+        $this->checks[$type][] = ['type' => $matchType, 'value' => $value, 'addl' => $args];
     }
 
     public function getChecks()
@@ -154,15 +234,20 @@ class User
 // -------------------------
 $enforcer = new Enforcer();
 $myUser = new User([
-    'username' => 'ccornutt',
+    'username' => ['ccornutt', 'ccornutt1'],
     'permissions' => ['test1']
 ]);
 
 $myPolicy = new Policy();
-$myPolicy->hasUsername('ccornutt')->notPermissions(['test']);
+// $myPolicy->hasUsername('ccornutt')->notPermissions(['test']);
+$myPolicy
+    // ->hasUsername(['ccornutt', 'ccornutt1'], Policy::ANY);
+    // ->notUsername(['ccornutt', 'ccornutt2'], Policy::ANY);
+    ->notUsername(['ccornutt2'], Policy::ANY);
+    // ->notPermissions(['test']);
 
-// print_r($myPolicy);
+print_r($myPolicy);
 
 $result = $enforcer->evaluate($myUser, $myPolicy);
-
 echo 'RESULT: '.var_export($result, true)."\n\n";
+
