6ac989ed2e64f20967497d70cbd3639c4c06319a
Chris Cornutt <enygma@phpdeveloper.org>
2015-12-23 07:34:09 -0600

    adding Gateway class for a simpler interface

---
+++ src/Gateway.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Psecio\PropAuth;
+use Psecio\PropAuth\Subject;
+use Psecio\PropAuth\Authenticator;
+use Psecio\PropAuth\PolicySet;
+
+class Gateway
+{
+    protected $subject;
+    protected $policies;
+
+    public function __construct($subject, Context $context = null)
+    {
+        $this->subject = ($subject instanceof Subject) ? $subject : new Subject($subject);
+        $this->policies = new PolicySet();
+
+        if ($context->get('policies') !== null) {
+            $this->setupPolicies($context->get('policies'));
+        }
+    }
+
+    public function setupPolicies($policies)
+    {
+        if ($policies instanceof PolicySet) {
+            $this->policies = $policies;
+        } else {
+            foreach ($policies as $name => $policy) {
+                $this->policies->add($name, $policy);
+            }
+        }
+    }
+
+    public function authenticate($password)
+    {
+        $auth = Authenticator::make(
+            'password',
+            ['password' => $password, 'subject' => $this->subject]
+        );
+        $result = $auth->evaluate();
+        if ($result === true) {
+            $this->subject->setAuth(true);
+            return $this->subject;
+        }
+        return false;
+    }
+
+    public function evaluate($policyName = null)
+    {
+        // Find the policy by name
+        $policy = $this->policies[$policyName];
+        if ($policy === null) {
+            throw new \InvalidArgumentException('Invalid policy: '.$policyName);
+        }
+        if ($this->subject->isAuthed() === false) {
+            throw new \InvalidArgumentException('You cannot perform policy evaluations on a non-authenticated subject.');
+        }
+        $enforcer = new Enforcer();
+        return $enforcer->evaluate($this->subject, $policy);
+    }
+}
